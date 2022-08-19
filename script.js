7ce54f9fefa236be4504576d275d39101736beed
adamvaughn <adamdvaughn@gmail.com>
2022-08-19 14:17:37 -0500

    animation and answers given

---
+++ script.js
@@ -0,0 +1,27 @@
+function shake(){
+
+    var ball= document.getElementById("ball")
+    var messageText = document.getElementById("message")
+ 
+    if(messageText != null){
+     messageText.parentNode.removeChild(messageText);
+    }
+    
+    ball.classList.add("shake");
+ 
+    setTimeout(function(){ ball.classList.remove("shake"); }, 1500);
+    
+    setTimeout(function(){ getFortune(); }, 1500);
+ }
+ 
+ function getFortune(){
+     var fortunes = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes -- definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes, Signs point to yes', 'Reply hazy', 'try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+ 
+     var fortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+ 
+     var parent = document.getElementById("fortune");
+     var newMessage = document.createElement("div");
+     newMessage.setAttribute('id', "message");
+     newMessage.innerHTML = "\""+fortune+"\"";
+     parent.appendChild(newMessage);
+ }
